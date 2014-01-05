5319965b22d3d78daef7ebccc787bccb40a36eea
njncalub <njncalub@gmail.com>
2014-01-05 10:12:09 +0800

    updated multi

--- res/js/multi.js
+++ res/js/multi.js
@@ -2,18 +2,21 @@
   var sync_object;
 
   sync_object = {
+    "GDrive": {
+      "key": "842525491470-nrqu90td4m8udnt4vcpqr95ok0fqtaht.apps.googleusercontent.com",
+      "scope": "https://www.googleapis.com/auth/drive",
+      "app_name": "The Hidden Cloud Academy"
+    },
     "Dropbox": {
       "key": "sbgjel5lku38766",
       "secret": "tu2qyn94u01k7p0",
       "app_name": "The Hidden Cloud Academy"
     },
-    "GDrive": {
-      "key": "424243246254-n6b2v8j4j09723ktif41ln247n75pnts.apps.googleusercontent.com",
-      "scope": "https://www.googleapis.com/auth/drive",
-      "app_name": "diary_app"
-    },
   };
 
   Nimbus.Auth.setup(sync_object);
+
+  // var sync_string = "eyJHRHJpdmUiOnsia2V5IjoiODQyNTI1NDkxNDcwLW5ycXU5MHRkNG04dWRudDR2Y3Bxcjk1b2swZnF0YWh0LmFwcHMuZ29vZ2xldXNlcmNvbnRlbnQuY29tIiwic2NvcGUiOiJodHRwczovL3d3dy5nb29nbGVhcGlzLmNvbS9hdXRoL2RyaXZlIiwiYXBwX25hbWUiOiJUaGUgSGlkZGVuIENsb3VkIEFjYWRlbXkifSwiRHJvcGJveCI6eyJrZXkiOiJzYmdqZWw1bGt1Mzg3NjYiLCJzZWNyZXQiOiJ0dTJxeW45NHUwMWs3cDAiLCJhcHBfbmFtZSI6IlRoZSBIaWRkZW4gQ2xvdWQgQWNhZGVteSJ9fQ==";
+  // Nimbus.Auth.setup(sync_string);
 
 }).call(this);
